90c9970f45b0dbb2e4f589f871d1648f4cecabdb
douglas <ds_cc_95@hotmail.com>
2015-09-16 17:14:56 -0400

    agregar menu

--- calculadora/calculadora/Program.cs
+++ calculadora/calculadora/Program.cs
@@ -12,6 +12,23 @@ namespace calculadora
         {
             Console.WriteLine("hello world");
             Console.ReadKey();
+            int op;
+            float x, y, z;
+            do
+            {
+                Console.WriteLine("elija una operacion");
+                Console.WriteLine("1 suma");
+                Console.WriteLine("2 resta");
+                Console.WriteLine("3 multiplicacion");
+                Console.WriteLine("4 division");
+                Console.WriteLine("salir");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+ 
+                }
+            } while (op != 5);
+
         }
     }
 }
